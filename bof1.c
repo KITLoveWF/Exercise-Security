318b7cfa4541d38972bb8331799635740fe35dfc
Mai Đức Kiên <119934394+KITLoveWF@users.noreply.github.com>
2024-09-09 20:22:43 +0700

    Add files via upload

---
+++ bof1.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<unistd.h>
+void secretFunc()
+{
+    printf("Congratulation!\n:");
+}
+int vuln(){
+    char array[200];
+    printf("Enter text:");
+    gets(array);
+    return 0;
+}
+int main(int argc, char*argv[]){
+    if (argv[1]==0){
+        printf("Missing arguments\n");
+    }
+    vuln();
+    return 0;
+}
